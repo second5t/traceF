076d8959508d19f705f18f9063adda8d52eb8e9c
M-ouse <mouseisnotalwaysmouse@outlook.com>
2025-02-09 21:27:20 -0700

    add get pin scirpt

---
+++ get-pin.sh
@@ -0,0 +1,3 @@
+wget https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.28-98749-g6643ecee5-gcc-linux.tar.gz
+tar -xvf pin-3.28-98749-g6643ecee5-gcc-linux.tar.gz
+mv pin-3.28-98749-g6643ecee5-gcc-linux pin
