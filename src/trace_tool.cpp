0bcf62a999e66a61a637973e98497f5797ae7792
M-ouse <mouseisnotalwaysmouse@outlook.com>
2025-03-09 11:17:31 -0600

    opt with cache

--- src/trace_tool.cpp
+++ src/trace_tool.cpp
@@ -3,67 +3,74 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 const std::vector<std::string> excludeList = {
     ".plt", "sanitizer", "asan", "@plt", "__lsan", "__interceptor",
-    "__interception", "cmplog", "__afl", "sancov", "ubsan"
+    "__interception", "cmplog", "__afl", "sancov", "ubsan", "fuzzer"
 };
-
 class FunctionTracer {
 public:
-    std::vector<std::string> functionNames; // 存储调用的函数名（允许重复）
+    std::vector<const std::string*> functionNames;
 
-    void AddFunction(const std::string& funcName) {
+    FunctionTracer() {
+        functionNames.reserve(1000000);
+    }
+
+    void AddFunction(const std::string* funcName) {
         functionNames.push_back(funcName);
     }
 
     void DumpToFile(const std::string& filename) {
         std::ofstream outFile(filename);
-        for (const auto& name : functionNames) {
-            outFile << name << std::endl;
+        for (const auto* name : functionNames) {
+            outFile << *name << std::endl;
         }
         outFile.close();
     }
 };
 
-FunctionTracer tracer;  // 创建一个全局对象
+FunctionTracer tracer;
 
-// 该函数在每次函数调用时执行
-VOID FunctionEntry(VOID* ip, std::string* name) {
-    // std::cout << "Function called: " << *name << " at address: " << ip << std::endl;
-    tracer.AddFunction(*name);
+std::unordered_map<ADDRINT, const std::string*> funcNameCache;
+
+VOID FunctionEntry(VOID* ip, const std::string* name) {
+    tracer.AddFunction(name);
 }
 
 bool shouldExclude(const std::string& name) {
     return name.empty() || std::any_of(excludeList.begin(), excludeList.end(),
-                                       [&name](const std::string& pattern) {
-                                           return name.find(pattern) != std::string::npos;
-                                       });
+        [&name](const std::string& pattern) {
+            return name.find(pattern) != std::string::npos;
+        });
 }
 
-// 对每个函数进行插桩
 VOID InstrumentFunction(IMG img, VOID* v) {
-    bool isMainImg = IMG_IsMainExecutable(img);
-    if (!isMainImg) {
+    if (!IMG_IsMainExecutable(img)) {
         return;
     }
     for (SEC sec = IMG_SecHead(img); SEC_Valid(sec); sec = SEC_Next(sec)) {
         for (RTN rtn = SEC_RtnHead(sec); RTN_Valid(rtn); rtn = RTN_Next(rtn)) {
             RTN_Open(rtn);
-
-            std::string* funcName = new std::string(RTN_Name(rtn));
-            if (shouldExclude(*funcName)) {
+            std::string rtnName = RTN_Name(rtn);
+            if (shouldExclude(rtnName)) {
                 RTN_Close(rtn);
                 continue;
             }
-            // std::cout << "Instrumenting function: " << *funcName << " isMainImg: " << isMainImg << std::endl;
+            ADDRINT addr = RTN_Address(rtn);
+            const std::string* cachedName = nullptr;
+            auto it = funcNameCache.find(addr);
+            if (it == funcNameCache.end()) {
+                cachedName = new std::string(rtnName);
+                funcNameCache[addr] = cachedName;
+            } else {
+                cachedName = it->second;
+            }
             
-            // 在函数入口插桩
             RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)FunctionEntry,
                            IARG_INST_PTR,
-                           IARG_PTR, funcName,
+                           IARG_PTR, cachedName,
                            IARG_END);
-
             RTN_Close(rtn);
         }
     }
@@ -82,7 +89,6 @@ VOID ThreadFini(THREADID tid, const CONTEXT* ctxt, INT32 code, VOID* v) {
     WriteToFile();
 }
 
-// Pin 工具的主入口
 int main(int argc, char* argv[]) {
     if (PIN_Init(argc, argv)) {
         std::cerr << "This is a function tracing tool using Pin." << std::endl;
@@ -95,6 +101,6 @@ int main(int argc, char* argv[]) {
     PIN_AddFiniFunction(Fini, 0);
     PIN_AddThreadFiniFunction(ThreadFini, 0);
 
-    PIN_StartProgram(); // 启动目标程序
+    PIN_StartProgram();
     return 0;
 }
